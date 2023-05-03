cc3f49cd61e56878f7bf0f7d24bc65d2c757d54d
Halyna Melnyk <39273210+mehalyna@users.noreply.github.com>
2023-05-03 14:11:48 +0300

    added pages and test for authorization

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,8 +2,26 @@
 # See https://docs.pytest.org/en/latest/ for more information
 
 import pytest
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from src.pages.common.BasePage import BasePage
+import config
 
 
-@pytest.fixture(scope="session")
-def scope_method():
-    pass
+@pytest.fixture(scope="function")
+def app():
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.maximize_window()
+    yield BasePage(driver)
+    driver.quit()
+
+
+@pytest.fixture(scope="function")
+def admin_setup(app):
+    """
+        Login as an admin
+    """
+    app.go_to_site()
+    app.landing.sign_up_btn.click_btn_by_css()
+    app.modal.login(config.ADMIN_EMAIL, config.ADMIN_PASS)
+    return app
