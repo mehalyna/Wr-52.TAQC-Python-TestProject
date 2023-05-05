96a71165734f716c71c3f90518c69c71dfc29115
Victoria Teltova <v.teltova@gmail.com>
2023-05-05 12:51:01 +0200

    changed config

--- config.py
+++ config.py
@@ -3,6 +3,6 @@
 """
 
 BASE_URL = "https://eventsexpress-test.azurewebsites.net/"
-ADMIN_EMAIL = "AlexTestQC@gmail.com"
-ADMIN_PASS = "123456"
+#ADMIN_EMAIL = "somemail@gmail.com"
+#ADMIN_PASS = "somepassword"
 LANDING_SIGN_IN_BUTTON = "Sign In"
