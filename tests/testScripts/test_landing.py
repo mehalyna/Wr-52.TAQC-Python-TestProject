03168aa8284bc1018ff98b54edfcbb45f45a2e36
Nikita Sherstianykh <n.sherstianykh@gmail.com>
2023-05-06 23:05:29 +0200

    Adding my tests for sing_in with correct credentionals and sing_up with incorrect credentionals

--- tests/testScripts/test_landing.py
+++ tests/testScripts/test_landing.py
@@ -1,7 +1,10 @@
 # It'll be the importing of report tools here
 
 import config
+import time
 import pytest
+from src.resources.config_credentials import irina_email, irina_password, irina_account_name, wrong_email, \
+    wrong_password
 
 """
     Testing the 'Landing' page
@@ -28,3 +31,26 @@ def test_landing_registration(app):
     app.modal.registration(config.ADMIN_EMAIL, config.ADMIN_PASS)
     assert expected_result == app.modal.get_success_register_text(), \
         "alert message is not the same as expected"
+
+
+def test_registration_form_appears_after_click(app):
+    app.landing.go_to_site()
+    app.landing.sign_up_btn.click_btn_by_css()
+    registration_form = app.find_element_by_xpath(app.modal.FORM_PAGE_XPATH)
+    assert registration_form
+
+
+def test_sign_in_with_right_credentials(app):
+    app.landing.go_to_site()
+    app.landing.sign_up_btn.click_btn_by_css()
+    app.modal.login(irina_email, irina_password)
+    assert irina_account_name == app.navigation.get_user_name()
+
+
+def test_sign_up_with_incorrect_data(app):
+    expected_result = "Must be 6 characters or more"
+    app.landing.go_to_site()
+    app.landing.sign_up_btn.click_btn_by_css()
+    app.modal.registration(wrong_email, wrong_password)
+    error_msg = app.modal.find_element_by_xpath(app.modal.UNSUCCESS_PAGE_ALERT_TEXT_XPATH)
+    assert error_msg.text == expected_result
