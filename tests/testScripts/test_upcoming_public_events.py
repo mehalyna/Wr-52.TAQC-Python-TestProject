659f7da193b9697fd44c0a0e562bdfd6dc347b86
Victoria Teltova <v.teltova@gmail.com>
2023-05-08 10:53:59 +0200

    changed according to review

--- tests/testScripts/test_upcoming_public_events.py
+++ tests/testScripts/test_upcoming_public_events.py
@@ -1,19 +1,13 @@
 import config
 import pytest
 
-
-
 """
     Testing the upcoming public events
 """
-
 
 def test_upcoming_public_events_guest(app):
     app.landing.go_to_site()
     assert app.landing.get_upcoming_public_event()
 
-
 def test_upcoming_public_events_after_login(admin_setup):
     assert admin_setup.landing.get_upcoming_public_event()
-
-
